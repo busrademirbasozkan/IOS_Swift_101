217c1597c8dad90126e5554b32e6e293b85124ac
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-09-19 21:45:55 +0300

    SegueApp dosyası eklendi.

---
+++ SegueApp/SegueApp/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SegueApp
+//
+//  Created by Büşra Özkan on 19.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var VCLabel: UILabel!
+    @IBOutlet weak var nameTextField : UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func nextButton(_sender:UIButton)
+    {
+        
+    }
+}
+
