0e9d4a3a62fec3524e95d73bda637e043dbb188f
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-09-19 20:58:01 +0300

    NoteTaker dosyası eklendi.

---
+++ NoteTaker/NoteTaker/Controller/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  NoteTaker
+//
+//  Created by Büşra Özkan on 18.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let storedName = UserDefaults.standard.object(forKey: "Name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "Birthday")
+        
+        if let name = storedName as? String {
+            nameLabel.text = "Name: \(name)"
+        }
+        if let birthday = storedBirthday as? String {
+            birthdayLabel.text = "Birthday: \(birthday)"
+        }
+        
+        
+    }
+
+    @IBAction func saveButton(_sender: UIButton){
+        UserDefaults.standard.set(nameTextField.text, forKey: "Name")
+        UserDefaults.standard.set(birthdayTextField.text, forKey: "Birthday")
+        
+        nameLabel.text = "Name: \(nameTextField.text!)"
+        birthdayLabel.text = "Birthday: \(birthdayTextField.text!)"
+        
+    }
+
+}
+
